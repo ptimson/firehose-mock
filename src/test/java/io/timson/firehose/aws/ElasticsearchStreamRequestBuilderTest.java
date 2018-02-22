d1c19147ee4c590911187032a2b75fc39f064ab9
Grizwold <tomekbielaszewski@gmail.com>
2018-02-22 20:41:09 +0100

    Create elastic stream delivery request - additional fields and tests

--- src/test/java/io/timson/firehose/aws/ElasticsearchStreamRequestBuilderTest.java
+++ src/test/java/io/timson/firehose/aws/ElasticsearchStreamRequestBuilderTest.java
@@ -1,7 +1,54 @@
 package io.timson.firehose.aws;
 
-import static org.junit.Assert.*;
+import com.amazonaws.services.kinesisfirehose.model.ElasticsearchDestinationConfiguration;
+import org.junit.Test;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
+import static org.hamcrest.Matchers.nullValue;
 
 public class ElasticsearchStreamRequestBuilderTest {
-    //TODO
+
+    @Test
+    public void shouldCreateElasticsearchConfig_WhenAllParamsProvided() throws Exception {
+        ElasticsearchStreamRequestBuilder builder = new ElasticsearchStreamRequestBuilder();
+
+        ElasticsearchDestinationConfiguration config = builder
+                .withIndexName("index")
+                .withTypeName("type")
+                .withDomainARN("domainARN")
+                .withroleARN("roleARN")
+                .withIndexRotationPeriod("rotation")
+                .withBufferSizeMb(1)
+                .withBufferIntervalSeconds(2)
+                .build();
+
+        assertThat(config.getIndexName(), is("index"));
+        assertThat(config.getTypeName(), is("type"));
+        assertThat(config.getIndexRotationPeriod(), is("rotation"));
+        assertThat(config.getDomainARN(), is("domainARN"));
+        assertThat(config.getRoleARN(), is("roleARN"));
+        assertThat(config.getBufferingHints().getSizeInMBs(), is(1));
+        assertThat(config.getBufferingHints().getIntervalInSeconds(), is(2));
+    }
+
+    @Test
+    public void shouldCreateElasticsearchConfig_WhenBufferParamsMissing() throws Exception {
+        ElasticsearchStreamRequestBuilder builder = new ElasticsearchStreamRequestBuilder();
+
+        ElasticsearchDestinationConfiguration config = builder
+                .withIndexName("index")
+                .withTypeName("type")
+                .withDomainARN("domainARN")
+                .withroleARN("roleARN")
+                .withIndexRotationPeriod("rotation")
+                .build();
+
+        assertThat(config.getIndexName(), is("index"));
+        assertThat(config.getTypeName(), is("type"));
+        assertThat(config.getIndexRotationPeriod(), is("rotation"));
+        assertThat(config.getDomainARN(), is("domainARN"));
+        assertThat(config.getRoleARN(), is("roleARN"));
+        assertThat(config.getBufferingHints(), is(nullValue()));
+    }
 }
